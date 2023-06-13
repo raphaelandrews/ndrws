60ff0224ff931d11e3c5b076f4845a62ecfcfe5a
Raphael Andrews <raphaelandrews95@gmail.com>
2023-06-13 00:25:38 -0300

    chore: project setup

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types/site";
+
+export const siteConfig: SiteConfig = {
+  name: "Raphael Andrews",
+  description:
+    "Fullstack Developer",
+  url: "https://ndrws.dev",
+  ogImage: "",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/raphaelandrews",
+  },
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        foreground: "hsl(var(--foreground))",
+        background: "hsl(var(--background))",
       },
     },
   },
